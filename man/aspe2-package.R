09df7266a839cbc037197ce5908439b9edbe9501
manue6 <112400613+manue6@users.noreply.github.com>
2024-03-24 01:12:02 +0100

    Add files via upload

---
+++ man/aspe2-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import magrittr
+#' @rawNamespace import(dplyr, except = lag)
+#' @import GFE
+#' @import lubridate
+#' @rawNamespace import(rlang, except = set_names)
+#' @import sf
+#' @rawNamespace import(stats, except = filter)
+#' @rawNamespace import(tidyr, except = extract)
+#' @import tidyselect
+#' @import utils
+#' @importFrom grDevices png postscript x11 dev.off
+#' @importFrom graphics par
+## usethis namespace: end
+NULL
